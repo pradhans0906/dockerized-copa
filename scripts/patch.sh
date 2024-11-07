38e6041ab1ae10faa193144f278fc96514cdc492
swapnasagar pradhan <swapnasagar.pradhan@gmail.com>
2024-11-07 00:36:27 +0000

    Fixing somr bugs

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -2,19 +2,31 @@
 
 # Check if image argument is provided
 if [ -z "$1" ]; then
-    echo "Usage: ./patch.sh <image:tag> [output-tag]"
+    echo "Usage: ./patch.sh <image:tag> [custom-tag-suffix]"
+    echo
+    echo "Examples:"
+    echo "  ./patch.sh nginx:1.21.6            # Will create nginx:1.21.6-patched"
+    echo "  ./patch.sh nginx:1.21.6 secure     # Will create nginx:1.21.6-secure"
     exit 1
 fi
 
 IMAGE="$1"
-TAG="${2:-patched}"
+TAG_SUFFIX="${2:-patched}"  # Default to 'patched' if no suffix provided
 
-# Run COPA
-docker run --rm --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -e DOCKER_CONFIG=/root/.docker \
-    copa-local:0.9.0 \
-    "$IMAGE" \
-    "$TAG"
+# Run COPA with tag argument only if custom suffix is provided
+if [ "$TAG_SUFFIX" != "patched" ]; then
+    docker run --rm --privileged \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -e DOCKER_CONFIG=/root/.docker \
+        copa-local:0.9.0 \
+        "$IMAGE" \
+        "$TAG_SUFFIX"
+else
+    docker run --rm --privileged \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -e DOCKER_CONFIG=/root/.docker \
+        copa-local:0.9.0 \
+        "$IMAGE"
+fi
 
 echo "Done! Check 'docker images' for the patched image."
